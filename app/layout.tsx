54560f62555ba56e4a022ee088f49f68f74018c6
Kevin Hernandez <kevinhdezvaz@gmail.com>
2026-04-12 14:10:57 -0600

    ADD - Google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -134,6 +134,8 @@ export default async function RootLayout({
           <Footer />
           <CookieBanner />
         </NextIntlClientProvider>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-9M4HGJ6F6P" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">{`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-9M4HGJ6F6P');`}</Script>
         <Script id="meta-pixel" strategy="afterInteractive">{`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1437786764820297');fbq('track','PageView');`}</Script>
         <noscript><img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=1437786764820297&ev=PageView&noscript=1" alt="" /></noscript>
       </body>
